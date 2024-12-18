b5f27a435cd68646bcb8c976412898ed651ab44d
devtot0 <lifesim11@gmail.com>
2024-12-19 00:17:32 +0100

    resource development

--- app/Filament/Resources/DepartmentResource.php
+++ app/Filament/Resources/DepartmentResource.php
@@ -19,6 +19,10 @@ class DepartmentResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+    protected static ?string $navigationGroup = 'System Management';
+
+    protected static ?int $navigationSort = 4;
+
     public static function form(Form $form): Form
     {
         return $form
--- app/Filament/Resources/StateResource.php
+++ app/Filament/Resources/StateResource.php
@@ -19,6 +19,12 @@ class StateResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+    protected static ?string $navigationLabel = 'State';
+
+    protected static ?string $navigationGroup = 'System Management';
+
+    protected static ?int $navigationSort = 2;
+
     public static function form(Form $form): Form
     {
         return $form
